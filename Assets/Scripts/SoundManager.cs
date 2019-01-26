4cf580d3eafc13460ea10d268062a45a206f8539
zmienny <kamil.roszak98@gmail.com>
2019-01-26 20:13:18 +0100

    Add Menu and Sounds

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance;
+    public AudioMixer audioMixer;
+
+    
+    float musicVolume = 1f;
+    float soundVolume = 1f;
+
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Awake()
+    {
+        LoadSavedValues();
+    }
+
+    private void OnDestroy()
+    {
+        SaveValues();
+    }
+
+    public void ChangeMusicVolume(float newValue)
+    {
+        musicVolume = newValue;
+        SaveValues();
+        CommitVolumeChanges();
+    }
+
+    public void ChangeSoundVolume(float newValue)
+    {
+        soundVolume = newValue;
+        SaveValues();
+        CommitVolumeChanges();
+    }
+
+    void LoadSavedValues()
+    {
+        musicVolume = PlayerPrefs.GetFloat("musicVolume", 1f);
+        soundVolume = PlayerPrefs.GetFloat("soundVolume", 1f);
+    }
+
+    void SaveValues()
+    {
+        PlayerPrefs.SetFloat("musicVolume", musicVolume);
+        PlayerPrefs.SetFloat("soundVolume", soundVolume);
+    }
+
+    void CommitVolumeChanges()
+    {
+        audioMixer.SetFloat("musicVolume", musicVolume);
+    }
+}
---
+++ Assets/Scripts/VolumeChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeChanger : MonoBehaviour {
+
+    public Slider slider;
+
+    enum WhatToDo
+    {
+        UpdateSoundVolume,
+        UpdateMusicVolume
+    }
+
+    [SerializeField]
+    WhatToDo whatToDo;
+
+    public void ValueHasChanged()
+    {
+        switch(whatToDo)
+        {
+            case WhatToDo.UpdateMusicVolume:
+                SoundManager.instance.ChangeMusicVolume(slider.value);
+                break;
+            case WhatToDo.UpdateSoundVolume:
+                SoundManager.instance.ChangeSoundVolume(slider.value);
+                break;
+        }
+    }
+}
